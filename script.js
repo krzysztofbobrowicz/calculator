f6bb8062bf53a5e0dab6ae8450b447eba68f0198
Krzysztof Bobrowicz <kbobrowicz98@gmail.com>
2022-09-15 17:31:25 +0200

    Create operator function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const add = (num1, num2) => {
-  return num1 + num2;
+  if (num1 !== NaN && num2 !== NaN) {
+    return num1 + num2;
+  }
+  return 'Naaa!';
 };
 
 const subtract = (num1, num2) => {
@@ -14,3 +17,21 @@ const divide = (num1, num2) => {
   return num1 / num2;
 };
 
+const operate = (operator, num1, num2) => {
+  switch (operator) {
+    case '+':
+      console.log(add(num1, num2));
+      break;
+    case '-':
+      console.log(subtract(num1, num2));
+      break;
+    case '/':
+      console.log(divide(num1, num2));
+      break;
+    case '*':
+      console.log(multiply(num1, num2));
+      break;
+  }
+};
+
+operate('*', 5, 4);
