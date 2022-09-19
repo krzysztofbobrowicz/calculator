2fa7f385def5295b0ea2fa66cfb7c96f4aa02c43
Krzysztof Bobrowicz <kbobrowicz98@gmail.com>
2022-09-19 10:04:30 +0200

    Create add functionality

--- script.js
+++ script.js
@@ -1,37 +1,71 @@
+const numberButtons = document.querySelectorAll('.num');
+const operationButtons = document.querySelectorAll('.operation');
+const equalsButton = document.querySelector('#equals');
+const clearButton = document.querySelector('#clear');
+let result = document.querySelector('#result');
+let firstNumber = '';
+let secondNumber = 0;
+
+result.innerText = '0';
+let displayValue = '';
+let storedNumbers = [0, 0];
+let chosenOperation = '';
+
+numberButtons.forEach(number =>
+  number.addEventListener('click', () => {
+    result.innerText === '';
+    displayValue += number.innerText;
+    result.innerText = displayValue;
+  })
+);
+
+operationButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if (displayValue !== '') {
+      storedNumbers[0] = displayValue;
+    }
+    if (btn.innerHTML === '-' && result.innerHTML === '0') {
+      displayValue = '-';
+      result.innerHTML = '-';
+    } else {
+      result.innerHTML = '-';
+      displayValue = '';
+    }
+    if (btn.innerHTML === '+') {
+      firstNumber = storedNumbers[0];
+      chosenOperation = '+';
+      console.log(firstNumber);
+      displayValue = '';
+      result.innerHTML = '+';
+    }
+  });
+});
+
+equalsButton.addEventListener('click', () => {
+  storedNumbers[1] = displayValue;
+  secondNumber = storedNumbers[1];
+  if (storedNumbers[1] !== '' && !isNaN(storedNumbers[0])) {
+    operate(chosenOperation, firstNumber, secondNumber);
+    storedNumbers = [];
+    console.log(storedNumbers);
+  } else return;
+});
+
 const add = (num1, num2) => {
-  if (num1 !== NaN && num2 !== NaN) {
-    return num1 + num2;
-  }
-  return 'Naaa!';
+  console.log('hello');
+  let finalResult = parseInt(num1) + parseInt(num2);
+  result.innerHTML = finalResult;
+  displayValue = '';
 };
 
-const subtract = (num1, num2) => {
-  return num1 - num2;
-};
+const subtract = (num1, num2) => {};
 
-const multiply = (num1, num2) => {
-  return num1 * num2;
-};
+const multiply = (num1, num2) => {};
 
-const divide = (num1, num2) => {
-  return num1 / num2;
-};
+const divide = (num1, num2) => {};
 
 const operate = (operator, num1, num2) => {
-  switch (operator) {
-    case '+':
-      console.log(add(num1, num2));
-      break;
-    case '-':
-      console.log(subtract(num1, num2));
-      break;
-    case '/':
-      console.log(divide(num1, num2));
-      break;
-    case '*':
-      console.log(multiply(num1, num2));
-      break;
-    default:
-      console.log('Sorry, there is something wrong with your statement.');
+  if (operator === '+') {
+    add(num1, num2);
   }
 };
