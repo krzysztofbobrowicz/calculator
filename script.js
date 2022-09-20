29a780bc6978c8371f3f7d8a13a11a308b631932
Krzysztof Bobrowicz <kbobrowicz98@gmail.com>
2022-09-20 12:35:16 +0200

    Finish all logic

--- script.js
+++ script.js
@@ -1,71 +1,93 @@
-const numberButtons = document.querySelectorAll('.num');
-const operationButtons = document.querySelectorAll('.operation');
-const equalsButton = document.querySelector('#equals');
-const clearButton = document.querySelector('#clear');
-let result = document.querySelector('#result');
-let firstNumber = '';
-let secondNumber = 0;
-
-result.innerText = '0';
-let displayValue = '';
-let storedNumbers = [0, 0];
-let chosenOperation = '';
-
-numberButtons.forEach(number =>
-  number.addEventListener('click', () => {
-    result.innerText === '';
-    displayValue += number.innerText;
-    result.innerText = displayValue;
-  })
-);
-
-operationButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    if (displayValue !== '') {
-      storedNumbers[0] = displayValue;
-    }
-    if (btn.innerHTML === '-' && result.innerHTML === '0') {
-      displayValue = '-';
-      result.innerHTML = '-';
-    } else {
-      result.innerHTML = '-';
-      displayValue = '';
-    }
-    if (btn.innerHTML === '+') {
-      firstNumber = storedNumbers[0];
-      chosenOperation = '+';
-      console.log(firstNumber);
-      displayValue = '';
-      result.innerHTML = '+';
-    }
-  });
-});
-
-equalsButton.addEventListener('click', () => {
-  storedNumbers[1] = displayValue;
-  secondNumber = storedNumbers[1];
-  if (storedNumbers[1] !== '' && !isNaN(storedNumbers[0])) {
-    operate(chosenOperation, firstNumber, secondNumber);
-    storedNumbers = [];
-    console.log(storedNumbers);
-  } else return;
-});
-
-const add = (num1, num2) => {
-  console.log('hello');
-  let finalResult = parseInt(num1) + parseInt(num2);
-  result.innerHTML = finalResult;
-  displayValue = '';
+const calculator = {
+  displayValue: '0',
+  firstNumber: null,
+  waitingForNextNumber: false,
+  operator: null,
 };
 
-const subtract = (num1, num2) => {};
+const updateScreen = () => {
+  const { displayValue } = calculator;
+  document.querySelector('#result').value = displayValue;
+};
 
-const multiply = (num1, num2) => {};
-
-const divide = (num1, num2) => {};
-
-const operate = (operator, num1, num2) => {
-  if (operator === '+') {
-    add(num1, num2);
+const insertNum = digit => {
+  const { displayValue, waitingForNextNumber } = calculator;
+  if (waitingForNextNumber) {
+    calculator.displayValue = digit;
+    calculator.waitingForNextNumber = false;
+  } else {
+    calculator.displayValue =
+      displayValue === '0' && !isNaN(displayValue)
+        ? digit
+        : displayValue + digit;
   }
 };
+
+const handleOperator = nextOperator => {
+  const { waitingForNextNumber, displayValue, firstNumber, operator } =
+    calculator;
+  if (operator && waitingForNextNumber) {
+    return (calculator.operator = nextOperator);
+  }
+  calculator.waitingForNextNumber = true;
+  calculator.firstNumber = displayValue;
+  calculator.operator = nextOperator;
+  if (operator) {
+    const result = calculate(
+      parseFloat(firstNumber),
+      parseFloat(displayValue),
+      operator
+    );
+    calculator.displayValue = result;
+    calculator.firstNumber = result;
+  }
+};
+
+const calculate = (firstNumber, secondNumber, operator) => {
+  switch (operator) {
+    case '+':
+      return firstNumber + secondNumber;
+    case '-':
+      return firstNumber - secondNumber;
+    case '/':
+      return firstNumber / secondNumber;
+    case '*':
+      return firstNumber * secondNumber;
+  }
+  return firstNumber;
+};
+
+const insertDecimal = decimal => {
+  !calculator.displayValue.includes(decimal)
+    ? (calculator.displayValue += decimal)
+    : null;
+};
+
+const clearScreen = () => {
+  calculator.displayValue = '0';
+  calculator.firstNumber = null;
+  calculator.waitingForNextNumber = false;
+  calculator.operator = null;
+};
+
+updateScreen();
+document.querySelector('#buttons').addEventListener('click', e => {
+  const { target } = e;
+  if (target.classList.contains('num')) {
+    insertNum(target.value);
+    updateScreen();
+  }
+  if (target.classList.contains('operator')) {
+    handleOperator(target.value);
+    updateScreen();
+  }
+  if (target.classList.contains('dot')) {
+    insertDecimal(target.value);
+    updateScreen();
+  }
+  if (target.classList.contains('clear')) {
+    clearScreen();
+    updateScreen();
+  }
+  return;
+});
